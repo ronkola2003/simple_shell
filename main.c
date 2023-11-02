81c96374c890330c8f09abafcacc17fcb17e4a5a
ronkola2003 <oyebeautypalace@gmail.com>
2023-11-01 19:40:45 -0700

    main.c

--- main.c
+++ main.c
@@ -41,45 +41,47 @@ void handle_ctrl_c(int opr UNUSED)
  * @env: environ passed to program execution
  * @argc: number of values received from comand line
  */
-void initialize_data(data_of_program *data, int arc, char *argv[], char **env)
+void initialize_data(data_of_program *data, int argc, char *argv[], char **env)
 {
-int i = 0;
+	int i = 0;
+	
+	data->program_name = argv[0];
+	data->input_line = NULL;
+	data->command_name = NULL;
+	data->exec_counter = 0;
+	/***** defines the file descriptor to be read *****/
 
-data->program_name = argv[0];
-data->input_line = NULL;
-data->command_name = NULL;
-data->exec_counter = 0;
-/***** defines the file descriptor to be read *****/
-if (arc == 1)
-data->file_descriptor = STDIN_FILENO;
-else
-{
-data->file_descriptor = open(argv[1], O_RDONLY);
-if (data->file_descriptor == -1)
-{
-_printe(data->program_name);
-_printe(": 0: Can't open ");
-_printe(argv[1]);
-_printe("\n");
-exit(127);
-}
-}
-data->tokens = NULL;
-data->env = malloc(sizeof(char *) * 50);
-if (env)
-{
-for (; env[i]; i++)
-{
-data->env[i] = str_duplicate(env[i]);
-}
-}
-data->env[i] = NULL;
-env = data->env;
-data->alias_list = malloc(sizeof(char *) * 20);
-for (i = 0; i < 20; i++)
-{
-data->alias_list[i] = NULL;
-}
+	if (argc == 1)
+
+		data->file_descriptor = STDIN_FILENO;
+	else
+	{
+		data->file_descriptor = open(argv[1], O_RDONLY);
+		if (data->file_descriptor == -1)
+		{
+			_printe(data->program_name);
+			_printe(": 0: Can't open ");
+			_printe(argv[1]);
+			_printe("\n");
+			exit(127);
+		}
+	}
+	data->tokens = NULL;
+	data->env = malloc(sizeof(char *) * 50);
+	if (env)
+	{
+		for (; env[i]; i++)
+		{
+			data->env[i] = str_duplicate(env[i]);
+		}
+	}
+	data->env[i] = NULL;
+	env = data->env;
+	data->alias_list = malloc(sizeof(char *) * 20);
+	for (i = 0; i < 20; i++)
+	{
+		data->alias_list[i] = NULL;
+	}
 }
 
 
